b9dc743a06869fc3d70b656500316da491be3c31
Breanna Badalov <BreannaBadalov@gmail.com>
2020-05-28 12:34:18 -0400

    Initial Commit

---
+++ Table View Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Table View Challenge
+//
+//  Created by admin on 5/28/20.
+//  Copyright © 2020 Breanna Badalov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
